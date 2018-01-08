9475aa422a471e9331e6010573817129e057cd49
bamboolsu <bamboolsu@gmail.com>
2018-01-08 18:33:10 +0800

    添加注释；

--- springboot-mybatis-tx/src/main/java/com/forezp/service/AccountService2.java
+++ springboot-mybatis-tx/src/main/java/com/forezp/service/AccountService2.java
@@ -14,9 +14,13 @@ public class AccountService2 {
     @Autowired
     AccountMapper2 accountMapper2;
 
+    /*@Transactional，声明事务，并设计一个转账方法，用户1减10块，用户2加10块。在用户1减10 ，之后，抛出异常，即用户2加10块钱不能执行，
+    当加注解@Transactional之后，两个人的钱都没有增减。
+    当不加@Transactional，用户1减了10，用户2没有增加，即没有操作用户2 的数据。
+    可见@Transactional注解开启了事物。*/
     @Transactional
-    public void transfer() throws RuntimeException{
-        accountMapper2.update(90,1);//用户1减10块 用户2加10块
+    public void transfer() throws RuntimeException {
+        accountMapper2.update(90, 1);//用户1减10块 用户2加10块
         int i=1/0;
         accountMapper2.update(110,2);
     }
